ab0dba34f2a62bac9a56021ba90986498ea6e0f4
Miguel-Viloria <2024-2-00905@lpunetwork.edu.ph>
2024-10-24 17:37:54 +0800

    Add files via upload

---
+++ Viloria_Lab9.py
@@ -0,0 +1,11 @@
+
+Row = int(input("Enter the number of rows"))
+number = 1
+for cr in range(1, Row + 1 ):
+    for r in range (1, cr + 1):
+        print(number, end=" ")
+        number += 1
+    print("")
+      
+       
+
